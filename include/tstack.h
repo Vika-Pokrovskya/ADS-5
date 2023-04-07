a851839bba088de266715c407927e82ed493e62c
Vika-Pokrovskya <117218299+Vika-Pokrovskya@users.noreply.github.com>
2023-04-07 22:53:29 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,31 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+   private:
+  T mass[100];
+  int a;
+
+ public:
+  TStack() : a(-1) {}
+   
+  const T& pop() {
+    if ( empty()) {
+      throw std::string("empty!!!!!");
+    } else {
+      return mass[a--];
+    }
+  }
+  void push(T value) {
+    if (full()) {
+      throw std::string("It is full(((");
+    } else {
+     mass[++a] = value;
+    }
+  }
+  
+  const T& poll() const { return mass[a]; }
+  bool empty() const { return a == -1; }
+  bool full() const { return a == size - 1; }
 };
 
 #endif  // INCLUDE_TSTACK_H_
