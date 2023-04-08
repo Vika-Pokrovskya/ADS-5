889bf3cfbabb7bdf8a815817ec9d1404fe9e1ad0
Vika-Pokrovskya <117218299+Vika-Pokrovskya@users.noreply.github.com>
2023-04-08 14:44:23 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,8 +1,9 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
  private:
   T mass[size] = {0};
